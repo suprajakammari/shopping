debcec0b4855bf2bc8bf18d2acc558b4d983fd25
jithenderkonduri420 <Jithender.Konduri@trigyn.com>
2022-02-02 13:13:27 +0530

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseURL: "http://ec2-3-111-53-18.ap-south-1.compute.amazonaws.com/live/api",
+  serverUploads: "http://ec2-3-111-53-18.ap-south-1.compute.amazonaws.com/live",
 };
